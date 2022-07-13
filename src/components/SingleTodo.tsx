f5ab230d1afc89e3cc8fcefb1e4a4396b7406a5a
Anshuman Pradhan <me.anshu7@gmail.com>
2022-07-13 17:31:24 +0530

    added edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
@@ -23,22 +23,39 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         setTodos(todos.filter(todo => todo.id !== id));
     };
 
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTodos(
+            todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+        );
+        setEdit(false);
+    };
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit])
+
     return (
-        <form className="todos__single">
+        <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
             {
                 edit ? (
-                    <input value={editTodo} />
+                    <input
+                        ref={inputRef}
+                        value={editTodo}
+                        onChange={(e) => setEditTodo(e.target.value)}
+                        className="todo__single--text"
+                    />
                 ) : todo.isDone ? (
                     <s className="todos__single--text">{todo.todo}</s>
 
                 ) : (
                     <span className="todos__single--text">{todo.todo}</span>
-
                 )
             }
             <div>
                 <span className="icon" onClick={() => {
-                    if (!edit && todo.isDone) {
+                    if (!edit && !todo.isDone) {
                         setEdit(!edit)
                     }
                 }
